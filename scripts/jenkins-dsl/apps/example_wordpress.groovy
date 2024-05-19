caf600c3ef4789837dc92597c5d4cbaf96a91f18
HiepPM <hieppham.devops@gmail.com>
2024-05-20 00:40:07 +0700

    update groovy

--- scripts/jenkins-dsl/apps/example_wordpress.groovy
+++ scripts/jenkins-dsl/apps/example_wordpress.groovy
@@ -2,6 +2,7 @@ pipelineJob('example_wordpress') {
     definition {
         cps {
             script("""
+                
                 pipeline {
                     agent {
                         label 'default'
@@ -12,13 +13,18 @@ pipelineJob('example_wordpress') {
                                 git url: 'https://github.com/PhamMinhHiepIT2/jenkins-testing.git', branch: 'main'
                             }
                         }
-                        stage('Docker build') {
+                        stage('Docker build & push') {
                             steps {
                                 script {
-                                    sh '''
-                                        cd stack-example-wordpress
-                                        docker build .
-                                    '''
+                                    withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'DOCKER_PASS', usernameVariable: 'DOCKER_USER')]) {
+                                        sh '''
+                                            cd example-wordpress
+                                            docker build -t phamhiep/wordpress-example:latest .
+                                            docker login -u \${DOCKER_USER} -p \${DOCKER_PASS}
+                                            docker push phamhiep/wordpress-example:latest
+                                            
+                                        '''
+                                    }
                                 }
                             }
                         }
